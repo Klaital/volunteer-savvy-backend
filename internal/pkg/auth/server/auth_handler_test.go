1872075e135f6e79a284a9c485e0cf3909eb23e8
Kit Cox <chriscox@abandonedfactory.net>
2020-08-04 10:04:39 -0700

    Updating auth handler test to a suite

--- internal/pkg/auth/server/auth_handler_test.go
+++ internal/pkg/auth/server/auth_handler_test.go
@@ -1,35 +1,86 @@
 package server
 
 import (
+	"fmt"
+	"github.com/emicklei/go-restful"
+	_ "github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	"github.com/klaital/volunteer-savvy-backend/internal/pkg/config"
+	"github.com/klaital/volunteer-savvy-backend/internal/pkg/testhelpers"
 	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestAuthServer_GrantTokenHandler(t *testing.T) {
+type AuthServerTestSuite struct {
+	suite.Suite
+	Config *config.ServiceConfig
+	Container *restful.Container
+}
 
-	server := New(&config.ServiceConfig{
+// TestAuthHandlerTestSuite is the "main" entry point for the suite.
+func TestAuthHandlerTestSuite(t *testing.T) {
+	testSuite := new(AuthServerTestSuite)
+	suite.Run(t, testSuite)
+}
+
+func (suite *AuthServerTestSuite) SetupAllSuite() {
+	// TODO: launch the test database server with docker
+	err := testhelpers.InitializeDatabase(suite.Config.GetDbConn(), "file://internal/pkg/auth/migrations/", "file://internal/pkg/auth/testdata/")
+	if err != nil {
+		suite.T().Fatalf("Error initializing the db %v", err)
+		return
+	}
+
+	// Initialize the webservice
+	cfg := config.ServiceConfig{
 		BasePath:            "/vs",
 		ServiceVersion:      "0.0.0",
 		DatabaseHost:        "localhost",
 		DatabaseDriver:      "postgres",
-		DatabaseUser:        "vstester",
+		DatabaseUser:        "vstest",
 		DatabasePassword:    "rootpw",
 		DatabasePort:        5432,
 		DatabaseName:        "vstest",
 		LogLevel:            "debug",
 		LogStyle:            "prettyjson",
 		Port:                8080,
-		HealthCheckPath:     "/healthz",
+		StaticContentPath:   "/web",
+		SwaggerFilePath:     "/apidocs.json",
+		APIPath:             "",
+		SwaggerPath:         "/apidocs",
+		HealthCheckPath:     "/GetServiceStatus",
 		Logger:              log.NewEntry(log.New()),
-		BcryptCost:          0,
+		BcryptCost:          1,
 		TokenExpirationTime: "4h",
-		JwtPrivateKey:       "", // TODO
-		JwtPublicKey:        "", // TODO
-	})
-	handler := http.HandlerFunc(server.GrantTokenHandler)
+		JwtPrivateKey:       "", // TODO:
+		JwtPublicKey:        "", // TODO:
+	}
+	testSuite := new(AuthServerTestSuite)
+	testSuite.Config = &cfg
+	server := New(&cfg)
+	suite.Container = restful.NewContainer()
+	suite.Container.Add(server.GetAuthAPI())
+}
+
+// Perform initialization required by each test function
+func (suite *AuthServerTestSuite) BeforeTest(suiteName, testName string) {
+	// For some reason, testify runs the BeforeTest hook before the suite-wide SetupAllSuite,
+	// so we must hook on it manually here
+	if suite.Config.GetDbConn() == nil {
+		suite.SetupAllSuite()
+	}
+
+	// Run some handcrafted SQL to inject common test data from the top-level testdata directory
+	err := testhelpers.LoadFixtures(suite.Config.GetDbConn(), "file://internal/pkg/auth/testdata/")
+	if err != nil {
+		suite.T().Fatalf("Failed to load fixtures: %v", err)
+	}
+}
+
+func (suite *AuthServerTestSuite) TestAuthServer_GrantTokenHandler(t *testing.T) {
 
 	// Pre-declare these to make copy/pasting the test blocks easier
 	var resp *httptest.ResponseRecorder
@@ -39,31 +90,35 @@ func TestAuthServer_GrantTokenHandler(t *testing.T) {
 	//
 	// No Basic Auth included in request
 	//
+	fmt.Println("Starting test 1")
 	resp = httptest.NewRecorder()
 	req, err = http.NewRequest(http.MethodPost, "/vs/auth/token", nil)
+	fmt.Println("Request/response created")
 	if err != nil {
-		t.Fatal(err)
+		suite.T().Fatal(err)
 	}
-	handler.ServeHTTP(resp, req)
+	fmt.Println("Dispatching request")
+	suite.Container.Dispatch(resp, req)
 
+	fmt.Println("Testing response")
 	if resp.Code != http.StatusUnauthorized {
-		t.Errorf("GrantTokenHandler returned wrong status code: Expected %v want %v",
+		suite.T().Errorf("GrantTokenHandler returned wrong status code: Expected %v want %v",
 			resp.Code, http.StatusUnauthorized)
 	}
 
 	//
 	// Incorrect basic auth included in request
 	//
-	resp = httptest.NewRecorder()
-	req, err = http.NewRequest(http.MethodPost, "/vs/auth/token", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	handler.ServeHTTP(resp, req)
-
-	if resp.Code != http.StatusUnauthorized {
-		t.Errorf("GrantTokenHandler returned wrong status code: Expected %v want %v",
-			resp.Code, http.StatusUnauthorized)
-	}
+	//resp = httptest.NewRecorder()
+	//req, err = http.NewRequest(http.MethodPost, "/vs/auth/token", nil)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//suite.Container.Dispatch(resp, req)
+	//
+	//if resp.Code != http.StatusUnauthorized {
+	//	t.Errorf("GrantTokenHandler returned wrong status code: Expected %v want %v",
+	//		resp.Code, http.StatusUnauthorized)
+	//}
 
 }
